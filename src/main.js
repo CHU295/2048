ec4db06d71f2c07bd532a536056b649cf9a87495
chu <873056154@qq.com>
2017-12-06 18:06:12 +0800

    添加了redux字段支持chrome检测

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore } from 'redux'
 import HomePage from './view/homePage'
 
 import Reducers from '../store/index'
@@ -9,9 +9,7 @@ import Reducers from '../store/index'
 import './assets/css/defalut.scss'
 import './assets/css/main.scss'
 
-let createStoreWithMiddleware = applyMiddleware()(createStore)
-
-const store = createStore(Reducers)
+const store = createStore(Reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDom.render(
   <Provider store={store}>
